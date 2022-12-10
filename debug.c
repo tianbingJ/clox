4718fab8dc856d33f92cbe778512e06d8b4c9734
tianbing <tbbd426@gmail.com>
2022-12-11 02:23:44 +0800

    add disassemble chunk

---
+++ debug.c
@@ -0,0 +1,29 @@
+//
+// Created by tblyl on 2022/12/11.
+//
+#include "debug.h"
+#include <stdio.h>
+
+void disassembleChunk(Chunk *chunk, const char * name) {
+    printf("== %s ==\n", name);
+    for (int offset = 0; offset < chunk->count;) {
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+static int simpleInstruction(const char * name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+int disassembleInstruction(Chunk * chunk, int offset) {
+    printf("%04d ", offset);
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown code %d\n", offset);
+            return offset + 1;
+    }
+}
---
+++ debug.h
@@ -0,0 +1,14 @@
+//
+// Created by tblyl on 2022/12/11.
+//
+
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char * name);
+
+int disassembleInstruction(Chunk * chunk, int offset);
+
+#endif //CLOX_DEBUG_H
